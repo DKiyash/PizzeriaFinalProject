de4ad9efad3644696a1f4d51788d3839eb7ea3f3
DKiyash <dkiyashenko@gmail.com>
2023-03-15 10:47:19 +0100

    First commit

---
+++ src/main/java/de/telran/pizzeriaproject/PizzeriaProjectApplication.java
@@ -0,0 +1,13 @@
+package de.telran.pizzeriaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzeriaProjectApplication.class, args);
+    }
+
+}
