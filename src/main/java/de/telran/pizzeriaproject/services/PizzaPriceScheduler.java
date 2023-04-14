36d138c7f5292b84c57a93bdb7471240f88f78cb
DKiyash <dkiyashenko@gmail.com>
2023-04-14 10:00:42 +0200

    Added Javadoc`s in PizzaPriceScheduler

--- src/main/java/de/telran/pizzeriaproject/services/PizzaPriceScheduler.java
+++ src/main/java/de/telran/pizzeriaproject/services/PizzaPriceScheduler.java
@@ -5,15 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.DecimalFormat;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * The PizzaPriceScheduler class implements the scheduled task of discounting pizzas during lunchtime
- * <p>
  * and resetting the prices back to their initial values afterwards.
- * <p>
  * This class uses the PizzaService class to access and update pizza prices.
  *
  * @author Kiiashchenko Dmytro
@@ -22,8 +18,19 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class PizzaPriceScheduler {
 
+    /**
+     * This field contains the discount value. The discount value is a constant.
+     */
     private final Double LUNCH_TIME_DISCOUNT = 2.0;
+    /**
+     * This field contains a flag indicating whether the program was first run.
+     * In order that regardless of the server start-up time,
+     * the first method that reduces the cost is executed.
+     */
     private boolean firstLaunch = true;
+    /**
+     * This field is used to call methods of PizzaSersice class to work with a database.
+     */
     private final PizzaSersice pizzaSersice;
 
     public PizzaPriceScheduler(PizzaSersice pizzaSersice) {
@@ -33,12 +40,12 @@ public class PizzaPriceScheduler {
     /**
      * This method is used to discount the prices of pizzas at 13 o`clock, Monday to Friday.
      * It gets the list of all pizzas from the PizzaService and updates the prices by reducing a constant.
-     * If base price is null, it sets it to the current price of the pizza.
-     * Finally, it logs the price changes for each pizza and updates the database via the PizzaService.
+     * If the base price is less than the discount value, the base price is not changed.
+     * Finally, it updates the database via PizzaService and logs that all pizzas have changed prices.
      */
-    @Scheduled(cron = "0 * * * * MON-FRI", zone = "Europe/Berlin")
     //Выполнение каждую минуту в 00 сек с Пн по Пт (для теста)
-//    @Scheduled(cron = "0 0 13 * * MON-FRI", zone = "Europe/Berlin") //Выполнение в 13:00 каждый день с Пн по Пт
+//    @Scheduled(cron = "0 * * * * MON-FRI", zone = "Europe/Berlin")
+    @Scheduled(cron = "0 0 13 * * MON-FRI", zone = "Europe/Berlin") //Выполнение в 13:00 каждый день с Пн по Пт
     public void schedualePizzaPriceLanch() {
         List<Pizza> pizzaList = pizzaSersice.findAll();
         Double pizzaCurrentBasePrice;
@@ -58,13 +65,14 @@ public class PizzaPriceScheduler {
 
     /**
      * This method is used to set up the prices of pizzas after 14 o`clock, Monday to Friday.
+     * First, it is checked whether it was the first time the program was run
+     * so as not to increase the price above the basic one.
      * It gets the list of all pizzas from the PizzaService and sets the prices back to their initial values.
-     * If base price is null, it sets it to the current price of the pizza.
-     * Finally, it logs the price resets for each pizza and updates the database via the PizzaService.
+     * Finally, it updates the database via PizzaService and logs that all pizzas have changed prices.
      */
-    @Scheduled(cron = "30 * * * * MON-FRI", zone = "Europe/Berlin")
     //Выполнение каждую минуту в 30 сек с Пн по Пт (для теста)
-//    @Scheduled(cron = "0 0 14 * * MON-FRI", zone = "Europe/Berlin") //Выполнение в 14:00 каждый день с Пн по Пт
+//    @Scheduled(cron = "30 * * * * MON-FRI", zone = "Europe/Berlin")
+    @Scheduled(cron = "0 0 14 * * MON-FRI", zone = "Europe/Berlin") //Выполнение в 14:00 каждый день с Пн по Пт
     public void schedualePizzaPriceNoLanch() {
         //Если это первый запуск, то метод не должен увеличивать стоимость
         if (firstLaunch) {
